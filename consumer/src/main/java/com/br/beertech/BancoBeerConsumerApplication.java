e0af9eae77d60115b7fad2eb16f78a166f0c3973
Leandro Stefanini <leandro.stefanini@ab-inbev.com>
2020-10-10 10:03:14 -0300

    consumer init commit

---
+++ consumer/src/main/java/com/br/beertech/BancoBeerConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.br.beertech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancoBeerConsumerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BancoBeerConsumerApplication.class, args);
+  }
+
+}
